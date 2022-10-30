5d7a50abaaf1423fa3c17a6afc8a40423c710c4b
Mohammad Saad <saadsiraj254@gmail.com>
2022-10-30 23:27:13 +0800

    soem functions to convert a given number to its negative

---
+++ Codewars/Fundamentals/make negative/make_neg.py
@@ -0,0 +1,9 @@
+def make_negative( number ):
+    return -number if number>0 else number
+
+def make_negative( number ):
+    return -abs(number)
+
+def make_negative( number ):
+    return (-1) * abs(number)
+
